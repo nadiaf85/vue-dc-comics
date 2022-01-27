66f4f281744816453c7b5eddc67be938c65dbc79
nadiaf85 <nadia.fiorio@alice.it>
2022-01-27 16:04:30 +0100

    prova

---
+++ src/components/Header.vue
@@ -0,0 +1,106 @@
+<template>
+  <header>
+      <div class="container">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+            
+        </div>
+
+        <nav>
+          <ul>
+            <li v-for="(element, index) in links" :key="index">
+              <a href="#" :class="{'active': element.current}">{{element.text}}</a>
+          </li>
+          </ul>
+        </nav>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+      return{
+        links: [
+            {
+                text: "Characters",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "Comics",
+                url: "#",
+                current: true,
+            },
+            {
+                text: "Movies",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "TV",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "Games",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "Collectibles",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "Videos",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "Fans",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "News",
+                url: "#",
+                current: false,
+            },
+            {
+                text: "Shop",
+                url: "#",
+                current: false,
+            },
+        ]
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  
+  .logo{
+    display: flex;
+    width: 25%;
+  }
+
+  nav{
+
+    ul{
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    }
+
+    li a{
+      display: inline-block;
+      color: #000;
+      padding: 15px 20px;
+      text-decoration: none;
+      text-transform: uppercase;
+    }
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="container">
+      <section>
+        <h2> Content goes here </h2>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+  main{
+    background-color: black;
+    margin-top: 30px;
+    height: 100px;
+  }
+  h2{
+    color: white;
+    text-align: start;
+  }
+</style>
